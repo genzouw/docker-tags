79d31c514a59dcb596df7ccddb606aecd17b7e93
genzouw <genzouw@gmail.com>
2019-11-21 12:56:44 +0900

    Tune performance and print full size.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,23 @@
 DOCKER_IMAGE="${1}"
 
 # Prepend "library" string if a parameter is not contains "/" char.
+
 echo "${DOCKER_IMAGE}" \
   | grep '/' >/dev/null \
   || DOCKER_IMAGE="library/${DOCKER_IMAGE}"
 
 URL="https://registry.hub.docker.com/v2/repositories/${DOCKER_IMAGE}/tags/"
-while [ "${URL}" != null ]; do
-  RESPONSE="$(
-    curl --keepalive-time 300 -4 --silent "${URL}" 2>/dev/null
-  )"
-  URL="$(echo "${RESPONSE}" | jq -r '."next"')"
-  echo "${RESPONSE}"
-done \
-  | jq -r '."results"[] | .name+" "+.last_updated' \
-  | xargs -n 2 printf "%-20s\t%s\n"
+(
+  echo "TAG SIZE UPDATED"
+  echo "----- ----- -----"
+  while [ "${URL}" != null ]; do
+    RESPONSE="$(
+      curl --keepalive-time 300 -4 --silent "${URL}" 2>/dev/null
+    )"
+    URL="$(echo "${RESPONSE}" | jq -r '."next"')"
+    echo "${RESPONSE}" | jq -r '."results"[] | .name+" "+(.full_size|tostring)+" "+.last_updated' &
+  done
+  wait
+) | while read -r TAG SIZE LAST_UPDATED; do
+  printf "%-20s\t%10s\t%s\n" "${TAG}" "${SIZE}" "${LAST_UPDATED}"
+done
